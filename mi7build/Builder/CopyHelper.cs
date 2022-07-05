f784a2a09fe0458bb0b53a5feb265caf1380f45d
p149u3 <108482546+p149u3@users.noreply.github.com>
2022-07-05 20:39:05 +0300

    copy directory fix

--- mi7build/Builder/CopyHelper.cs
+++ mi7build/Builder/CopyHelper.cs
@@ -24,14 +24,17 @@ namespace mi7build {
 
             // Copy each file into the new directory.
             foreach (FileInfo fi in source.GetFiles()) {
-                fi.CopyTo(Path.Combine(target.FullName, fi.Name), true);
+                if (!fi.Name.StartsWith(".")) {
+                    fi.CopyTo(Path.Combine(target.FullName, fi.Name), true);
+                }
             }
 
             // Copy each subdirectory using recursion.
             foreach (DirectoryInfo diSourceSubDir in source.GetDirectories()) {
-                DirectoryInfo nextTargetSubDir =
-                    target.CreateSubdirectory(diSourceSubDir.Name);
-                CopyAll(diSourceSubDir, nextTargetSubDir);
+                if (!diSourceSubDir.Name.StartsWith(".")) {
+                    DirectoryInfo nextTargetSubDir = target.CreateSubdirectory(diSourceSubDir.Name);
+                    CopyAll(diSourceSubDir, nextTargetSubDir);
+                }
             }
         }
 
